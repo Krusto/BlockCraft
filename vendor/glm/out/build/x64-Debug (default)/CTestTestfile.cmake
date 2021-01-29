5b86755f98f1d95e65480510d951d39a421a681d
Krusto <kuckata2@gmail.com>
2021-01-29 11:26:41 +0200

    fix

---
+++ vendor/glm/out/build/x64-Debug (default)/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/KrustoPC/Documents/Projects/MinecraftClone/Fiko/vendor/glm
+# Build directory: C:/Users/KrustoPC/Documents/Projects/MinecraftClone/Fiko/vendor/glm/out/build/x64-Debug (default)
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
+subdirs("test")
